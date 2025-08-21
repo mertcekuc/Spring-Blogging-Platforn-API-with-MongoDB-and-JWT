960dd2008e87e9289165c93ecc1e74f4c962b8ff
mertcekuc <cekucmert@gmail.com>
2025-08-22 01:07:31 +0300

    first

---
+++ src/main/java/com/mertcekuc/blogg/ng/plkatform/BloggıngPlkatformApplication.java
@@ -0,0 +1,13 @@
+package com.mertcekuc.blogg.ng.plkatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloggıngPlkatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloggıngPlkatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mertcekuc/blogg/ng/plkatform/BloggıngPlkatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mertcekuc.blogg.ng.plkatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloggıngPlkatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
